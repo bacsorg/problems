f8cf82bd9929e2024df60e6017ee94f774478f4d
Aleksey Filippov <sarum9in@gmail.com>
2016-07-12 18:28:40 +0400

    Added SingleResultDecoder.

--- go/bacs/problem/decoder/single.go
+++ go/bacs/problem/decoder/single.go
@@ -6,6 +6,7 @@ import (
 )
 
 type singleTaskDecoder struct{}
+type singleResultDecoder struct{}
 
 func (_ singleTaskDecoder) DecodeToText(data []byte) (string, error) {
 	var task single.Task
@@ -16,8 +17,22 @@ func (_ singleTaskDecoder) DecodeToText(data []byte) (string, error) {
 	return proto.MarshalTextString(&task), nil
 }
 
+func (_ singleResultDecoder) DecodeToText(data []byte) (string, error) {
+	var result single.Result
+	err := proto.Unmarshal(data, &result)
+	if err != nil {
+		return "", err
+	}
+	return proto.MarshalTextString(&result), nil
+}
+
 func (d singleTaskDecoder) DecodeBase64ToText(data string) (string, error) {
 	return decodeBase64ToText(d, data)
 }
 
+func (d singleResultDecoder) DecodeBase64ToText(data string) (string, error) {
+	return decodeBase64ToText(d, data)
+}
+
 var SingleTaskDecoder singleTaskDecoder
+var SingleResultDecoder singleResultDecoder
--- go/bacs/problem/decoder/single_test.go
+++ go/bacs/problem/decoder/single_test.go
@@ -10,22 +10,45 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSingleTaskDecoder(t *testing.T) {
-	task := single.Task{
-		System: &problem.System{
-			ProblemType: "problem-type",
-			Package:     "package",
-		},
-	}
-	expectedText := `system: <
+func TestSingleDecoder(t *testing.T) {
+	testData := []struct {
+		pb           proto.Message
+		expectedText string
+		decoder      HumanDecoder
+	}{
+		{
+			pb: &single.Task{
+				System: &problem.System{
+					ProblemType: "problem-type",
+					Package:     "package",
+				},
+			},
+			expectedText: `system: <
   problem_type: "problem-type"
   package: "package"
 >
-`
-	data, err := proto.Marshal(&task)
-	require.NoError(t, err)
-	text, err := SingleTaskDecoder.DecodeToText(data)
-	if assert.NoError(t, err) {
-		assert.Equal(t, expectedText, text)
+`,
+			decoder: SingleTaskDecoder,
+		},
+		{
+			pb: &single.Result{
+				System: &problem.SystemResult{
+					Status: problem.SystemResult_INVALID_REVISION,
+				},
+			},
+			expectedText: `system: <
+  status: INVALID_REVISION
+>
+`,
+			decoder: SingleResultDecoder,
+		},
+	}
+	for _, tt := range testData {
+		data, err := proto.Marshal(tt.pb)
+		require.NoError(t, err)
+		text, err := tt.decoder.DecodeToText(data)
+		if assert.NoError(t, err) {
+			assert.Equal(t, tt.expectedText, text)
+		}
 	}
 }
