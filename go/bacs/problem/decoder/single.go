f49d3ea7a01e6b4c298cc268da736725a4db3009
Aleksey Filippov <sarum9in@gmail.com>
2016-07-21 17:17:52 +0400

    Added single.ProfileExtension decoding to decoder.singleTaskDecoder.

--- go/bacs/problem/decoder/single.go
+++ go/bacs/problem/decoder/single.go
@@ -10,11 +10,28 @@ type singleResultDecoder struct{}
 
 func (_ singleTaskDecoder) DecodeToText(data []byte) (string, error) {
 	var task single.Task
+	var profileExtension single.ProfileExtension
 	err := proto.Unmarshal(data, &task)
 	if err != nil {
 		return "", err
 	}
-	return proto.MarshalTextString(&task), nil
+	if task.Profile != nil && task.Profile.Extension != nil &&
+		task.Profile.Extension.TypeUrl ==
+			"type.googleapis.com/bacs.problem.single.ProfileExtension" {
+		ext := task.Profile.Extension
+		task.Profile = nil
+		err = proto.Unmarshal(ext.Value, &profileExtension)
+		if err != nil {
+			return "", err
+		}
+	}
+	text := proto.MarshalTextString(&task)
+	profileExtensionText := proto.MarshalTextString(&profileExtension)
+	if profileExtensionText != "" {
+		text += "\n"
+		text += profileExtensionText
+	}
+	return text, nil
 }
 
 func (_ singleResultDecoder) DecodeToText(data []byte) (string, error) {
